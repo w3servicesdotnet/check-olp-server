4218fd2443b66bf7d43760aacc3a38e8e6f8fc77
W3SERVICES <32176465+w3servicesdotnet@users.noreply.github.com>
2020-04-21 09:53:18 +0530

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -3,11 +3,11 @@ GITRAW=https://raw.githubusercontent.com/tujuhion/openlitespeed-centos-autoinsta
 LSWSDIR=/usr/local/lsws
 
 mkdir /scripts
-wget -O /scripts/lscreate $GITRAW/scripts/lscreate
-wget -O /scripts/lsremove $GITRAW/scripts/lsremove
-wget -O /scripts/certbot $GITRAW/scripts/certbot
-wget -O /scripts/createdb $GITRAW/scripts/createdb
-wget -O /usr/bin/lsws $GITRAW/scripts/lsws
+wget -q -O /scripts/lscreate $GITRAW/scripts/lscreate
+wget -q -O /scripts/lsremove $GITRAW/scripts/lsremove
+wget -q -O /scripts/certbot $GITRAW/scripts/certbot
+wget -q -O /scripts/createdb $GITRAW/scripts/createdb
+wget -q -O /usr/bin/lsws $GITRAW/scripts/lsws
 
 chmod +x /usr/bin/lsws
 chmod +x /scripts/*
